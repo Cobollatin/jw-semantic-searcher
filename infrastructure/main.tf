4428aa4b38ae04937336f72cbacdac282f0f757f
Cobollatin <cobollatin@hotmail.com>
2024-05-01 22:24:08 +0200

    infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,13 +22,25 @@ resource "azurerm_virtual_network" "use2_main_vnet" {
   tags                = var.common_tags
 }
 
-resource "azurerm_subnet" "use2_as_subnet" {
+resource "azurerm_subnet" "use2_swb_subnet" {
   name                                      = "${var.app_name}-${var.location_short}-${var.environment_name}-as-subnet"
   resource_group_name                       = azurerm_resource_group.use2_main_rg.name
   virtual_network_name                      = azurerm_virtual_network.use2_main_vnet.name
   address_prefixes                          = ["10.0.0.0/24"]
   private_endpoint_network_policies_enabled = false
   service_endpoints                         = ["Microsoft.Web", ]
+}
+
+resource "azurerm_network_security_group" "use2_swa_nsg" {
+  name                = "${var.app_name}-${var.location_short}-${var.environment_name}-as-nsg"
+  location            = azurerm_resource_group.use2_main_rg.location
+  resource_group_name = azurerm_resource_group.use2_main_rg.name
+  tags                = var.common_tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "use2_as_subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.use2_swb_subnet.id
+  network_security_group_id = azurerm_network_security_group.use2_swa_nsg.id
 }
 
 ############################################################################################################################
