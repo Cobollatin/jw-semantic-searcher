8444c6b0521b038601065eab3cfbf1039b0640c3
Cobollatin <cobollatin@hotmail.com>
2024-05-02 13:14:54 +0200

    removed batch application

--- scripts/boostrap/booststrap.sh
+++ scripts/boostrap/booststrap.sh
@@ -12,6 +12,8 @@ set -e
 # It is assumed these scripts are located in the same directory as the current script.
 CREATE_AZURE_CREDENTIALS_SCRIPT="./create_azure_credentials.sh"
 CREATE_AZURE_STORAGE_ACCOUNT_SCRIPT="./create_azure_storage_account.sh"
+CREATE_AZURE_SUBSCRIPTION_NAMESPACES_REGISTRATION_SCRIPT="./create_azure_subscription_namespaces_registration.sh"
+CREATE_GITHUB_SECRET_SCRIPT="./create_github_secret.sh"
 
 # Make sure both scripts that are necessary for setting up the environment
 # are executable. This is a precaution to avoid run-time errors related to permissions.
@@ -26,6 +28,16 @@ $CREATE_AZURE_CREDENTIALS_SCRIPT
 echo "Creating Azure Storage Account..."
 # Execute the script to create the Azure Storage Account.
 $CREATE_AZURE_STORAGE_ACCOUNT_SCRIPT
+
+# Inform the user that the registration of Azure subscription namespaces is starting.
+echo "Registering Azure subscription namespaces..."
+# Execute the script to register Azure subscription namespaces.
+$CREATE_AZURE_SUBSCRIPTION_NAMESPACES_REGISTRATION_SCRIPT
+
+# Inform the user that the creation of the GitHub secret is starting.
+echo "Creating GitHub secret..."
+# Execute the script to create the GitHub secret.
+$CREATE_GITHUB_SECRET_SCRIPT
 
 # Indicate to the user that the bootstrap process is complete.
 echo "Bootstrap complete!"
