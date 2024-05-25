7a65d618b83c5ccdb7c034ecb405a8c513501d9f
Cobollatin <cobollatin@hotmail.com>
2024-05-25 18:51:27 +0200

    stuff

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -589,7 +589,7 @@ resource "azurerm_batch_pool" "use2_main_batch_pool" {
   resource_group_name = azurerm_resource_group.use2_main_rg.name
   account_name        = azurerm_batch_account.use2_main_batch.name
   node_agent_sku_id   = "batch.node.ubuntu 20.04"
-  vm_size             = "Standard_A1"
+  vm_size             = "Standard_A1_V2"
   metadata            = var.common_tags
   max_tasks_per_node  = 1
   storage_image_reference {
