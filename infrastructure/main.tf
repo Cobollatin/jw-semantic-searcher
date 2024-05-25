0aa0108acea672b4cc31df40f673ef8dee4c6da8
Cobollatin <cobollatin@hotmail.com>
2024-05-25 18:48:59 +0200

    stuff

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -178,15 +178,6 @@ resource "azurerm_log_analytics_workspace" "use2_main_law" {
   }
 }
 
-resource "azurerm_log_analytics_workspace_table" "use2_main_law_table" {
-  name         = "StorageBlobLogs"
-  workspace_id = azurerm_log_analytics_workspace.use2_main_law.id
-  plan         = "Basic"
-  # Error: cannot set retention_in_days because the retention is fixed at eight days on Basic plan
-  # retention_in_days       = 30
-  total_retention_in_days = 0
-}
-
 ############################################################################################################################
 # SWA
 
