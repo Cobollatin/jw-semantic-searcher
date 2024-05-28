50ca207cfecb5345f7b2f1d99946d70a015d8a81
jackdelaguilavilla03 <jackdelaguilavilla03@gmail.com>
2024-05-28 16:01:28 -0500

    fix

--- indexer/Program.cs
+++ indexer/Program.cs
@@ -1,45 +1,37 @@
-﻿using Indexer.Services;
-using Indexer.Models;
+﻿using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
 using Azure.Search.Documents.Indexes.Models;
 using Azure.Search.Documents.Models;
+using Indexer.Models;
 using System;
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Indexer
+string serviceName = Environment.GetEnvironmentVariable("AZURE_SEARCH_SERVICE_NAME");
+string indexName = Environment.GetEnvironmentVariable("AZURE_SEARCH_INDEX_NAME");
+string apiKey = Environment.GetEnvironmentVariable("AZURE_SEARCH_API_KEY");
+
+var searchService = new AzureSearchService(serviceName, indexName, apiKey);
+
+var index = new SearchIndex(indexName)
 {
-    class Program
+    Fields =
     {
-        static async Task Main(string[] args)
-        {
-            string serviceName = Environment.GetEnvironmentVariable("AZURE_SEARCH_SERVICE_NAME");
-            string indexName = Environment.GetEnvironmentVariable("AZURE_SEARCH_INDEX_NAME");
-            string apiKey = Environment.GetEnvironmentVariable("AZURE_SEARCH_API_KEY");
-
-            var searchService = new AzureSearchService(serviceName, indexName, apiKey);
-
-            var index = new SearchIndex(indexName)
-            {
-                Fields =
-                {
-                    new SimpleField("id", SearchFieldDataType.String) { IsKey = true, IsFilterable = true },
-                    new SearchableField("preview") { IsFilterable = true, IsSortable = true },
-                    new SearchableField("url") { IsFilterable = true, IsSortable = true }
-                }
-            };
-
-            await searchService.CreateOrUpdateIndexAsync(index);
-
-            var documents = new List<Document>
-            {
-                new Document { Id = "1", Preview = "This is a preview of document 1", Url = "http://example.com/1" },
-                new Document { Id = "2", Preview = "This is a preview of document 2", Url = "http://example.com/2" },
-                new Document { Id = "3", Preview = "This is a preview of document 3", Url = "http://example.com/3" }
-            };
-
-            await searchService.UploadDocumentsAsync(documents);
-
-            Console.WriteLine("Index updated successfully with mock data.");
-        }
+        new SimpleField("id", SearchFieldDataType.String) { IsKey = true, IsFilterable = true },
+        new SearchableField("preview") { IsFilterable = true, IsSortable = true },
+        new SearchableField("url") { IsFilterable = true, IsSortable = true }
     }
-}
+};
+
+await searchService.CreateOrUpdateIndexAsync(index);
+
+var documents = new List<Document>
+{
+    new Document { Id = "1", Preview = "This is a preview of document 1", Url = "http://example.com/1" },
+    new Document { Id = "2", Preview = "This is a preview of document 2", Url = "http://example.com/2" },
+    new Document { Id = "3", Preview = "This is a preview of document 3", Url = "http://example.com/3" }
+};
+
+await searchService.UploadDocumentsAsync(documents);
+
+Console.WriteLine("Index updated successfully with mock data.");
