f2a93e9cfa4ae91ef395db70e3657ef5795b694c
Cobollatin <cobollatin@hotmail.com>
2024-05-30 06:57:58 +0200

    model names

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -319,7 +319,6 @@ resource "azurerm_key_vault" "use2_main_kv" {
   tenant_id                     = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days    = 7
   purge_protection_enabled      = true
-  public_network_access_enabled = false
   sku_name                      = "standard"
   tags                          = var.common_tags
 
