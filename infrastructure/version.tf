be013159e4cce9f6b3ca76529a9bc9c8f0c8d710
Cobollatin <cobollatin@hotmail.com>
2024-06-23 05:57:58 +0200

    Provider version

--- infrastructure/version.tf
+++ infrastructure/version.tf
@@ -6,7 +6,7 @@ terraform {
     # Here, 'azurerm' is specified as a required provider.
     azurerm = {
       source  = "hashicorp/azurerm" # The 'source' attribute specifies the location from which Terraform should download the provider. In this case, it's from HashiCorp's official provider registry.
-      version = "3.107.0"           # The 'version' attribute locks the provider to version 3.94.0. This ensures consistency and predictability in deployments, as Terraform will use this specific version of the provider.
+      version = "3.109.0"           # The 'version' attribute locks the provider to version 3.94.0. This ensures consistency and predictability in deployments, as Terraform will use this specific version of the provider.
     }
 
     github = {
