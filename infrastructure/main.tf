df3c95580ab79ef4de81ccf330c9f77d28312555
Cobollatin <cobollatin@hotmail.com>
2024-11-20 07:52:34 +0100

    upgrade

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -449,15 +449,15 @@ resource "azurerm_search_service" "use2_main_ss" {
   customer_managed_key_enforcement_enabled = false
   public_network_access_enabled            = true
   local_authentication_enabled             = true
-  sku                                      = "free"
+  sku                                      = "standard"
   tags                                     = var.common_tags
-  # semantic_search_sku                      = "free"
+  semantic_search_sku                      = "free"
   # Error: `semantic_search_sku` can only be specified when `sku` is not set to "free"
   # Resource identity is not supported for the selected SKU
-  # identity {
-  #   # The only possible value is SystemAssigned.
-  #   type = "SystemAssigned"
-  # }
+  identity {
+    # The only possible value is SystemAssigned.
+    type = "SystemAssigned"
+  }
 }
 
 # resource "azurerm_subnet" "use2_ss_subnet" {
